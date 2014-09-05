520400b473d69558b57a3716e501551ea4451634
Taus Møller <taus.moeller@gmail.com>
2014-09-05 08:17:10 +0200

    * deleted obsolete elpa packages

--- elpa/irony-20140623.1413/irony-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "irony" "20140623.1413" "C/C++ minor mode powered by libclang"
-  '((cl-lib "0.5"))
-  :url "https://github.com/Sarcasm/irony-mode" :keywords
-  '("c" "convenience" "tools"))
